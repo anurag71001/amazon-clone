0b4c87564b00263a9665f4559e32d7dbe8ee85ec
Anurag Khanna <71001anu@gmail.com>
2022-11-28 03:01:46 +0530

    half buil done

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     images: {
         domains: ["pngimg.com", "fakestoreapi.com", "th.bing.com"]
-    }
+    },
+    env: {
+        stripe_public_key: process.env.STRIPE_PUBLIC_KEY
+    },
 }
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,74 +1,90 @@
-import Image from 'next/image'
-import React from 'react'
-import { useSelector } from 'react-redux'
-import Header from '../components/Header'
-import { selectItems, selectTotal } from '../slices/basketSlice'
-import CheckoutProduct from '../components/CheckoutProduct'
-import Currency from 'react-currency-formatter'
-// import { useSession } from 'next-auth/react'
+import Image from "next/image";
+import React from "react";
+import { useSelector } from "react-redux";
+import Header from "../components/Header";
+import { selectItems, selectTotal } from "../slices/basketSlice";
+import CheckoutProduct from "../components/CheckoutProduct";
+import Currency from "react-currency-formatter";
+import { useSession } from "next-auth/react";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
+
+const stripePromise = loadStripe(process.env.stripe_public_key);
 
 function Checkout() {
-    const items = useSelector(selectItems);
-    const total = useSelector(selectTotal);
-    const session = useSession();
+  const items = useSelector(selectItems);
+  const total = useSelector(selectTotal);
+  const { data: session, status } = useSession();
+  const createCheckoutSession = async () => {
+    const stripe = await stripePromise;
+
+    // call the backend to create a checkout session
+    const checkoutSession = await axios.post("/api/create-checkout-session", {
+      items: items,
+      email: session.user.email,
+    });
+  };
   return (
-    <div className='bg-gray-100'>
-        <Header/>
+    <div className="bg-gray-100">
+      <Header />
 
-        <main className='lg:flex max-w-screen-2xl mx-auto'>
+      <main className="lg:flex max-w-screen-2xl mx-auto">
         {/* Left */}
-            <div className='lg:flex-grow m-5 shadow sm'>
-                <Image
-                src="https://th.bing.com/th/id/OIP.djVn7-22UJ6uCPrVRwTUwQHaDP?pid=ImgDet&rs=1"
-                width={1020}
-                height={250}
-                objectFit='contain'
-                />  
-                <div className='flex flex-col p-5 space-y-10 bg-white'>
-                    <h1 className='text-3xl border-b pb-4'>
-                        {items.length ===0 ?'Your Amazon cart is empty.':'Shopping Cart'}
-                    </h1>
+        <div className="lg:flex-grow m-5 shadow sm">
+          <Image
+            src="https://th.bing.com/th/id/OIP.djVn7-22UJ6uCPrVRwTUwQHaDP?pid=ImgDet&rs=1"
+            width={1020}
+            height={250}
+            objectFit="contain"
+          />
+          <div className="flex flex-col p-5 space-y-10 bg-white">
+            <h1 className="text-3xl border-b pb-4">
+              {items.length === 0
+                ? "Your Amazon cart is empty."
+                : "Shopping Cart"}
+            </h1>
 
-                    {items.map((item,i) =>(
-                        <CheckoutProduct
-                            key={i}
-                            id={item.id}
-                            title={item.title}
-                            rating={item.rating}
-                            price={item.price}
-                            description={item.description}
-                            category={item.category}
-                            image={item.image}
-                            hasPrime={item.hasPrime}
-                        />
-                    ))}
-                </div>   
-             </div>
-
+            {items.map((item, i) => (
+              <CheckoutProduct
+                key={i}
+                id={item.id}
+                title={item.title}
+                rating={item.rating}
+                price={item.price}
+                description={item.description}
+                category={item.category}
+                image={item.image}
+                hasPrime={item.hasPrime}
+              />
+            ))}
+          </div>
+        </div>
 
         {/* Right */}
-            <div className='flex flex-col bg-white p-10 shadow-md'>
-                {items.length>0 && (
-                    <>
-                        <h2 className='whitespace-nowrap'>Subtotal ({items.length} items):
-                        <span className='font-bold'>
-                        <Currency
-                            quantity={total}
-                            currency='INR'
-                        />
-                        </span>
-
-                        </h2>
-                        <button className='button ,t-2'{...!session && 'from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed'}>
-                            {!session ? 'Sign in to checkout':'Proceed to checkout'}
-                        </button>
-                    </>
-                )}
+        <div className="flex flex-col bg-white p-10 shadow-md">
+          {items.length > 0 && (
+            <div>
+            <h2 className="whitespace-nowrap">Subtotal ({items.length} items):{" "}
+            <span className="font-bold">
+            <Currency 
+            quantity={total} 
+            currency='INR'
+            />
+            </span>
+            </h2>
+            <button 
+            role="link"
+            onClick={createCheckoutSession}
+            disabled={!session.user} 
+            className='button mt-2' {...!session.user && 'from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed'}>
+            {session.user ? 'Proceed to checkout':'Sign-in to checkout'}
+            </button>
             </div>
-        </main>
+          )}
+        </div>
+      </main>
     </div>
-  )
+  );
 }
-import { useSession } from 'next-auth/react'
 
-export default Checkout
+export default Checkout;
